e48001df768ee06b26afe8115aec5a865fbcf1be
Александр Колдин <koldinweb@gmail.com>
2026-03-26 03:15:28 +0300

    Сделал Шаг 2 - прописал файл Readme.md

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,28 @@
-export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+export interface IProdcut {
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number | null;
+}
 
-export interface IApi {
-    get<T extends object>(uri: string): Promise<T>;
-    post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
+export interface IOrederForm {
+  payment: string;
+  address: string;
+}
+
+export interface IContactForm {
+  email: string;
+  phone: string;
+}
+
+export interface I0rder extends IOrederForm, IContactForm {
+  total: number;
+  items: string[];
+}
+
+export interface IOrderResult {
+  id: string;
+  total: number;
 }
