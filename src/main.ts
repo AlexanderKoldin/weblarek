61f62f900e02d5e5db6d0a03c260f0c78a82c90b
Александр Колдин <koldinweb@gmail.com>
2026-03-26 03:53:49 +0300

    Шаг 3: Сделал main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
+import { BasketModel } from './components/Models/BasketModel';
+import { CatalogModel } from './components/Models/CatalogModel';
 import './scss/styles.scss';
+import { apiProducts } from './utils/data';
+
+const catalog = new CatalogModel();
+catalog.setItems(apiProducts.items);
+console.log('Загружено товаров:', catalog.getItems().length);
+
+const basket = new BasketModel();
+const firstProduct = catalog.getItems()[0];
+
+basket.add(firstProduct);
+
+console.log('В корзине товаров:', basket.getCount());
+console.log('Итоговая сумма:', basket.getTotal());
+
+if (basket.hasItem(firstProduct.id)) {
+  console.log('Товар успешно добавлен и найден в корзине!');
+}
